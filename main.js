089f740f6d7c1b7c060e2421100c49a6b768df05
Nitish Kumar <mintu.nitish@gmail.com>
2018-07-23 01:38:15 +0530

    PowerLevels

--- main.js
+++ main.js
@@ -4,14 +4,52 @@ const sprite = $('#sprite');
 const meter = $('#meter');
 const meterContainer = $('#meter-container');
 
+const powerLevels = {
+    100: {
+        current: 'base',
+        previous: null,
+        next: 'ssj'
+    },
+    1000: {
+        current: 'ssj',
+        previous: 'base',
+        next: null
+    }
+};
+
+const fillMeter = (level) => {
+    const limit = 100;
+
+    if(level >= limit) {
+        return;
+    }
+
+    const containerWidth = meterContainer.offsetWidth;
+    const newWidth = (level/limit) * containerWidth;
+
+    meter.style.width = `${newWidth}px`;
+}
+
 const main = () => {
     const { fromEvent } = rxjs;
+    const { filter, map, scan, tap } = rxjs.operators;
 
     const start = fromEvent(document, 'keydown');
     const end = fromEvent(document, 'keyup');
 
-    start.subscribe(() => {
-        sprite.classList.add('powerup');
+    start.pipe(
+        scan(level => level + 1, 1),
+        tap(level => {
+            console.log({ level });
+            sprite.classList.add('powerup');
+            fillMeter(level);
+        }),
+        map(level => powerLevels[level]),
+        filter(level => level && level.next)
+    )
+    .subscribe(({ current, next }) => {
+        sprite.classList.remove(current);
+        sprite.classList.add(next);
     });
 
     end.subscribe(() => {
